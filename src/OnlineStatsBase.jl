0044cab020d3999d64f4f64f0dfd03b6aef7054f
Josh Day <emailjoshday@gmail.com>
2017-06-25 15:24:58 -0700

    remove StatsBase

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -1,16 +1,10 @@
 module OnlineStatsBase
 
-import StatsBase
+export AbstractSeries, Series, OnlineStat, Weight
 
-export AbstractSeries, OnlineStat, StochasticStat, Weight, Series
-
-abstract type Weight end
+abstract type Weight                    end
 abstract type OnlineStat{INDIM, OUTDIM} end
-abstract type StochasticStat{I, O} <: OnlineStat{I, O} end
-
-
-#-----------------------------------------------------------------------# Series
-abstract type AbstractSeries end
+abstract type AbstractSeries            end
 
 struct Series{I, OS <: Union{Tuple, OnlineStat{I}}, W <: Weight} <: AbstractSeries
     weight::W
