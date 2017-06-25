6f8e2052488d3375449c35e95f870becf1dab8cb
Josh Day <emailjoshday@gmail.com>
2017-06-24 18:48:11 -0700

    update with series

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -2,11 +2,19 @@ module OnlineStatsBase
 
 import StatsBase
 
-export AbstractSeries, OnlineStat, StochasticStat, Weight
+export AbstractSeries, OnlineStat, StochasticStat, Weight, Series
 
 abstract type Weight end
 abstract type OnlineStat{INDIM, OUTDIM} end
 abstract type StochasticStat{I, O} <: OnlineStat{I, O} end
+
+
+#-----------------------------------------------------------------------# Series
 abstract type AbstractSeries end
+
+struct Series{I, OS <: Union{Tuple, OnlineStat{I}}, W <: Weight} <: AbstractSeries
+    weight::W
+    stats::OS
+end
 
 end
