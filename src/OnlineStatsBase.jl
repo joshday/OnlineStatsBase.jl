dbbf20f29a385de8efcadcf8de62ab9e73e23c29
Josh Day <emailjoshday@gmail.com>
2017-06-30 00:38:29 -0400

    move some things from OnlineStats to OnlineStatsBase

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -2,9 +2,26 @@ module OnlineStatsBase
 
 export AbstractSeries, Series, OnlineStat, Weight
 
-abstract type Weight                    end
+#-----------------------------------------------------------------------# Weight
+abstract type Weight end
+
+#-----------------------------------------------------------------------# OnlineStat
+"""
+`OnlineStat{I, O}` is an abstract type parameterized by the input and output
+type/dimension `I`.
+
+
+"""
 abstract type OnlineStat{INDIM, OUTDIM} end
-abstract type AbstractSeries            end
+
+Base.copy(o::OnlineStat) = deepcopy(o)
+Base.map(f::Function, o::OnlineStat) = f(o)
+Base.start(o::OnlineStat) = false
+Base.next(o::OnlineStat, state) = o, true
+Base.done(o::OnlineStat, state) = state
+
+#-----------------------------------------------------------------------# Series
+abstract type AbstractSeries end
 
 struct Series{I, OS <: Union{Tuple, OnlineStat{I}}, W <: Weight} <: AbstractSeries
     weight::W
