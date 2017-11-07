b056e6d39b9ca8b4f58d8cbdf370cfa7c7999e84
Josh Day <emailjoshday@gmail.com>
2017-11-07 16:57:34 -0500

    wip

--- src/series.jl
+++ src/series.jl
@@ -40,11 +40,12 @@ Series(wt::Weight, o::OnlineStat) = Series{input_ndims(o), typeof(o), typeof(wt)
 Series(o::OnlineStat, wt::Weight = default_weight(o)) = Series(wt, o)
 
 
-# # Init with data
-# function Series(y::Data, wt::Weight, o::OnlineStat...; dim::ObsDimension = Rows())
-#     s = Series(wt, o)
-#     fit!(s, y, dim)
-# end
+# Init with data
+function Series(y::Data, wt::Weight, o::OnlineStat)
+    s = Series(wt, o)
+    fit!(s, y)
+end
+Series(y::Data, o::OnlineStat) = Series(y, default_weight(o), o)
 # Series(wt::Weight, y::Data, o::OnlineStat; kw...) = Series(y, wt, o; kw...)
 # function Series(y::Data, o::OnlineStat...; dim::ObsDimension = Rows())
 #     s = Series(o...)
