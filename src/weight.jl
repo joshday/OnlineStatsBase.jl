d2ca732bf0d5a1e7a6178c3bad6b72ad59715440
Josh Day <emailjoshday@gmail.com>
2017-11-09 13:53:34 -0500

    weight docs

--- src/weight.jl
+++ src/weight.jl
@@ -7,47 +7,145 @@ end
 Base.copy(w::Weight) = deepcopy(w)
 
 #-----------------------------------------------------------------------# EqualWeight
+"""
+    EqualWeight()
+
+Equally weighted observations.  
+
+``γ(t) = 1 / t``
+
+# Example
+
+    Series(randn(100), EqualWeight(), Variance())
+"""
 struct EqualWeight <: Weight end
 (::EqualWeight)(n, n2=1) = n2 / n
+
 #-----------------------------------------------------------------------# ExponentialWeight
+"""
+    ExponentialWeight(λ::Float64)
+    ExponentialWeight(lookback::Int)
+
+Exponentially weighted observations.  
+
+``γ(t) = λ = 2 / (lookback + 1)``
+
+# Example
+
+    Series(randn(100), ExponentialWeight(), Variance())
+"""
 struct ExponentialWeight <: Weight 
     λ::Float64 
     ExponentialWeight(λ::Real = .1) = new(λ)
     ExponentialWeight(lookback::Integer) = new(2 / (lookback + 1))
 end
 (w::ExponentialWeight)(n, n2=1) = n == 1 ? 1.0 : w.λ
+
 #-----------------------------------------------------------------------# LearningRate
+"""
+    LearningRate(r = .6)
+
+Slowly decreasing weight.  
+
+``γ(t) = 1 / t^r``
+
+# Example
+
+    Series(randn(1000), LearningRate(.7), QuantileMM(), QuantileMSPI(), QuantileSGD())
+"""
 struct LearningRate <: Weight 
     r::Float64 
     LearningRate(r = .6) = new(r)
 end
 (w::LearningRate)(n, n2=1) = 1 / n ^ w.r
+
 #-----------------------------------------------------------------------# LearningRate2
+"""
+    LearningRate2(c = .5)
+
+Slowly decreasing weight.  
+
+``γ(t) = 1 / (1 + c * (t - 1))``
+
+# Example
+
+    Series(randn(1000), LearningRate2(.3), QuantileMM(), QuantileMSPI(), QuantileSGD())
+"""
 struct LearningRate2 <: Weight 
     c::Float64 
     LearningRate2(c = .5) = new(c)
 end
 (w::LearningRate2)(n, n2=1) = 1 / (1 + w.c * (n - 1))
+
 #-----------------------------------------------------------------------# HarmonicWeight
+"""
+    HarmonicWeight(a = 10.0)
+
+Weight determined by harmonic series.  
+
+``γ(t) = a / (a + t - 1)``
+
+# Example
+
+    Series(randn(1000), HarmonicWeight(), QuantileMSPI())
+"""
 struct HarmonicWeight <: Weight 
     a::Float64 
     HarmonicWeight(a = 10.0) = new(a)
 end
 (w::HarmonicWeight)(n, n2=1) = w.a / (w.a + n - 1)
+
 #-----------------------------------------------------------------------# McclainWeight
+"""
+    McclainWeight(α = .1)
+
+Weight which decreases into a constant.
+
+``γ(t) = γ(t-1) / (1 + γ(t) - α)``
+
+# Example
+
+    Series(randn(100), McclainWeight(), Mean())
+"""
 mutable struct McclainWeight <: Weight
     α::Float64
     last::Float64
     McclainWeight(α = .1) = new(α, 1.0)
 end
 (w::McclainWeight)(n, n2=1) = n == 1 ? 1.0 : (w.last = w.last / (1 + w.last - w.α))
+
 #-----------------------------------------------------------------------# Bounded
+"""
+    Bounded(w::Weight, λ::Float64)
+
+Bound the weight by a constant.
+
+``γ_bounded(t) = max(γ(t), λ)``
+
+# Example
+
+    Bounded(EqualWeight(), .1)
+"""
 struct Bounded{W <: Weight} <: Weight 
     weight::W 
     λ::Float64 
 end
 (w::Bounded)(n, n2=1) = max(w.λ, w.weight(n, n2))
+
 #-----------------------------------------------------------------------# Scaled
+"""
+    Scaled(w::Weight, λ::Float64)
+
+Scale a weight by a constant.
+
+``γ_scaled(t) = λ * γ(t)``
+
+# Example
+
+    Bounded(LearningRate(.5), .1)
+
+    Series(randn(1000), 2.0 * LearningRate(.9), QuantileMM())
+"""
 struct Scaled{W <: Weight} <: Weight
     weight::W 
     λ::Float64
