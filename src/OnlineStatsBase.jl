c2f58a3c7a170c67207c331e11a2c4883ddbec13
Josh Day <emailjoshday@gmail.com>
2017-11-20 18:22:13 -0500

    docstrings and cleanup

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -10,43 +10,58 @@ const Data = Union{ScalarOb, VectorOb, AbstractMatrix, XyOb}
 #-----------------------------------------------------------------------# OnlineStat
 abstract type OnlineStat{N} end
 
-"An OnlineStat which can be updated exactly."
-abstract type ExactStat{N}      <: OnlineStat{N} end
+#-----------------------------------------------------------------------# ExactStat
+"""
+An OnlineStat which can be updated exactly.  Subtypes of `ExactStat` use `EqualWeight()`
+as the default weight.
+"""
+abstract type ExactStat{N} <: OnlineStat{N} end
 
-"An OnlineStat which must be updated approximately."
+#-----------------------------------------------------------------------# StochasticStat
+"""
+An OnlineStat which must be approximated.  Subtypes of `StochasticStat` use 
+`LearningRate()` as the default weight.  Additionally, subtypes should be parameterized
+by an algorithm, which is an optional last argument.  For example:
+
+    OnlineStats.Quantile([.5, .8])
+    OnlineStats.Quantile([.5, .8], OnlineStats.SGD())
+"""
 abstract type StochasticStat{N} <: OnlineStat{N} end
 
+#-----------------------------------------------------------------------# _value
 # The default value(o) returns the first field
-"""
-    value(o::OnlineStat)
-
-Return the value of the OnlineStat.
-"""
 @generated function _value(o::OnlineStat)
     r = first(fieldnames(o))
     return :(o.$r)
 end
+
+#-----------------------------------------------------------------------# _fit!
 _fit!(o::OnlineStat, ob, γ::Float64) = error("typeof(o) needs method: OnlineStatsBase._fit!")
 
+#-----------------------------------------------------------------------# show
 function Base.show(io::IO, o::OnlineStat)
     print(io, name(o), "(")
     showcompact(io, _value(o))
     print(io, ")")
 end
 
+#-----------------------------------------------------------------------# ==
 function Base.:(==)(o1::OnlineStat, o2::OnlineStat)
     typeof(o1) == typeof(o2) || return false
     nms = fieldnames(o1)
     all(getfield.(o1, nms) .== getfield.(o2, nms))
 end
 
+#-----------------------------------------------------------------------# copy
 Base.copy(o::OnlineStat) = deepcopy(o)
 
+#-----------------------------------------------------------------------# merge
 function Base.merge!(o::T, o2::T, γ::Float64) where {T<:OnlineStat} 
     warn("Merging not well-defined for $(typeof(o)).  No merging occurred.")
 end
 Base.merge(o::T, o2::T, γ::Float64) where {T<:OnlineStat} = merge!(copy(o), o2, γ)
 
+#-----------------------------------------------------------------------# default_weight
 default_weight(o::OnlineStat)       = error("$(typeof(o)) has no `default_weight` method")
 default_weight(o::ExactStat)        = EqualWeight()
 default_weight(o::StochasticStat)   = LearningRate()
@@ -60,18 +75,15 @@ end
 
 #-----------------------------------------------------------------------# Weight
 """
-`Weight` is an abstract type.  Subtypes must be callable have a method to produce the
-weight given the current number of observations in an OnlineStat `n` and the number of 
-observations included in the update (`n2`).
+Subtypes of `Weight` must be callable to produce the weight given the current number of 
+observations in an OnlineStat `n` and the number of new observations (`n2`).
 
-```
-MyWeight(n, n2 = 1)
-```
+    MyWeight(n, n2 = 1)
 """
 abstract type Weight end 
 include("weight.jl")
 
-#-----------------------------------------------------------------------# helpers
+#-----------------------------------------------------------------------# name
 function name(o, withmodule = false, withparams = true)
     s = string(typeof(o))
     if !withmodule
@@ -85,4 +97,4 @@ function name(o, withmodule = false, withparams = true)
     s
 end
 
-end #module
+end
