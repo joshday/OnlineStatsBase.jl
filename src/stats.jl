25cb8e5e6a79b042e2b860bde003471651225b3c
joshday <emailjoshday@gmail.com>
2020-05-01 09:16:06 -0400

    docstring

--- src/stats.jl
+++ src/stats.jl
@@ -24,6 +24,14 @@ _merge!(a::Counter, b::Counter) = (a.n += b.n)
 Track a dictionary that maps unique values to its number of occurrences.  Similar to
 `StatsBase.countmap`.
 
+Counts can be incremented by values other than one (and decremented) using the `fit!(::CountMap{T}, ::Pair{T,Int})` method, e.g.
+
+```julia
+o = fit!(CountMap(String), ["A", "B"])
+fit!(o, "A" => 5)  
+fit!(o, "A" => -1)
+```
+
 # Example
 
     o = fit!(CountMap(Int), rand(1:10, 1000))
