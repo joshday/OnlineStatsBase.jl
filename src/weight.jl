4c66b29fd0a6ce41193c3fe6c09a391e3ee51eba
joshday <emailjoshday@gmail.com>
2020-10-05 16:36:38 -0400

    ExponentialWeight docstring

--- src/weight.jl
+++ src/weight.jl
@@ -24,7 +24,17 @@ struct EqualWeight <: Weight end
     ExponentialWeight(λ::Float64)
     ExponentialWeight(lookback::Int)
 
-Exponentially weighted observations.  Each weight is `λ = 2 / (lookback + 1)`.
+Exponentially weighted observations.  Each weight is `λ = 2 / (lookback + 1)`.  
+
+`ExponentialWeight` does not satisfy the usual assumption that `γ(1) == 1`.  Therefore, some
+statistics have an implicit starting value. 
+
+```
+# E.g. Mean has an implicit starting value of 0.
+o = Mean(weight=ExponentialWeight(.1))
+fit!(o, 10)
+value(o) == 1
+```
 
 ``γ(t) = λ``
 """
