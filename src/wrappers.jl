b4e488135eb6b0ee284343b32a51277a163ddf2a
joshday <emailjoshday@gmail.com>
2021-10-05 17:04:27 -0400

    docs

--- src/wrappers.jl
+++ src/wrappers.jl
@@ -89,9 +89,10 @@ Base.skipmissing(o::OnlineStat) = SkipMissing(o)
 """
     TryCatch(stat; error_limit=1000, error_message_limit=90)
 
-Wrap each call to `fit!` in a `try`-`catch` block and track the errors encountered (via [`CountMap`](@ref)).  Errors will stop
-being tracked after `error_limit` unique errors are encountered.  Only the first `error_message_limit`
-characters of each error message will be recorded.
+Wrap each call to `fit!` in a `try`-`catch` block and track the errors encountered (via [`CountMap`](@ref)).  
+Only `error_limit` unique errors will be included in the `CountMap`.  If a new error occurs after 
+`error_limit` has been reached, it will be included in the `CountMap` as `"Other"`.  Only the first 
+`error_message_limit` characters of each error message will be recorded.
 
 # Example 
 
