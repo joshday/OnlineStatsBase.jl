323ee7afc535651d7b59848e228b57f00e2809c1
joshday <emailjoshday@gmail.com>
2021-12-01 09:28:41 -0500

    docstring

--- src/stats.jl
+++ src/stats.jl
@@ -273,8 +273,19 @@ Base.minimum(o::Extrema) = o.min
 
 #-----------------------------------------------------------------------------# ExtremeValues
 """
+    ExtremeValues(T = Float64, b=25)
 
+Track the `b` smallest and largest values of a data stream (as well as how many times that value occurred).
 
+# Example 
+
+    o = ExtremeValues(Int, 3)
+
+    fit!(o, 1:100)
+
+    value(o).lo  # [1 => 1, 2 => 1, 3 => 1]
+
+    value(o).hi  # [98 => 1, 99 => 1, 100 => 1]
 """
 mutable struct ExtremeValues{T,S} <: OnlineStat{S}
     lo::Vector{Pair{T,Int}}
